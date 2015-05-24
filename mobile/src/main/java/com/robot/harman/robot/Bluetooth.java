545876a822df80bd1880d6f141905f288f279f97
sh953 <mannandd@gmail.com>
2015-05-23 21:13:37 -0400

    Android UI and Dummy Variable Integration
    Basic Android UI added. Dummy Variables set variable values onCreate
    only.

---
+++ mobile/src/main/java/com/robot/harman/robot/Bluetooth.java
@@ -0,0 +1,12 @@
+package com.robot.harman.robot;
+
+/**
+ * Created by Harman on 5/23/2015.
+ */
+public class Bluetooth extends MainActivity {
+
+    public static int light = 1;
+    public static int time = 1;
+    public static int temperature = 1;
+
+}
---
+++ mobile/src/main/java/com/robot/harman/robot/MainActivity.java
@@ -0,0 +1,51 @@
+package com.robot.harman.robot;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView temperature = (TextView) this.findViewById(R.id.temperatureValue);
+        temperature.setText(String.valueOf(Bluetooth.temperature));
+
+        TextView time = (TextView) this.findViewById(R.id.timeValue);
+        time.setText(String.valueOf(Bluetooth.time));
+
+        TextView light = (TextView) this.findViewById(R.id.lightValue);
+        light.setText(String.valueOf(Bluetooth.light));
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
